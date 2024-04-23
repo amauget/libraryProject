6a8977f2171e4075bb610fa2127d8d3d28faf251
AaronMauget <amauget@hotmail.com>
2024-04-23 15:05:24 -0700

    create grid layout add js function for read/notread button color place input @ location to prep for click to reveal change input class names via html

---
+++ README.md
@@ -0,0 +1,8 @@
+browser icon:
+    https://www.iconfinder.com/icons/9035645/book_sharp_icon
+
+camilla ahoy font:
+    https://www.dafont.com/camilla-ahoy.font
+
+caviar dreams font:
+    https://www.dafont.com/caviar-dreams.font
--- library.js
+++ library.js
@@ -1,54 +1,114 @@
 let output = document.querySelector('.outputText')
-const subButton = document.querySelector('.addItem');
+const subButton = document.querySelector('.addBtn');
+const inputs = document.querySelectorAll('input');
 let tracking = [] /* array to distinguish whether a book has been added. */
 
-subButton.addEventListener('click', buttonClick, true);
-function buttonClick(event){
+
+subButton.addEventListener('click', (event) =>{
+    event.preventDefault();
+    bookCreater();
+    inputs.forEach(input =>{input.value = ''});
+    // iterates over all input elements.
+    
+    
+});
+
+
+function bookCreater(){
+     /* Used to target "tracking" array items */
+
+    
         // HTML vars nested to access:
-    let title = (document.querySelector('.title')).value;
-    let author = (document.querySelector('.author')).value;
-    let pages = (document.querySelector('.pages')).value;
+    let title = (document.querySelector('.titleInpt')).value;
+    let author = (document.querySelector('.authorInpt')).value;
+    let pages = (document.querySelector('.pagesInpt')).value;
     let readStatus = (document.querySelector('.readStatus')).value;
 
-    if(title === null){
-        return null; /* Not catching non entries as it should. */
+    function Book(title, author, pages, readStatus){
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.readStatus = readStatus;
+        
     }
+        
+    Book.prototype.getBookInfo = function(){
+        let readButton = document.createElement('button');
+        readButton.textContent= this.readStatus;
+        let book = `Title: ${this.title} Author: ${this.author} Pages: ${this.pages}`;
+        return book;
+    }
+
+
+    let bookItem = new Book(title, author, pages, readStatus);
+    let titleItem = bookItem.title;
+    let authorItem = `By: ${bookItem.author}`;
+    let pagesItem = `${bookItem.pages} pages`;
+    let readItem = bookItem.readStatus;
+    if(!tracking.includes(bookItem.title)){ /* does not include */
+        tracking.push(bookItem.title);
+        bookOutput(titleItem, authorItem, pagesItem, readItem);
+        
+    }
+        
     else{
-        function Book(title, author, pages, readStatus){
-            this.title = title;
-            this.author = author;
-            this.pages = pages;
-            this.readStatus = readStatus;
-        }
-        
-        Book.prototype.getBookInfo = function(){
-            let book = `Title: ${this.title} Author: ${this.author} Pages: ${this.pages} Read Status: ${this.readStatus}`;
-            return book;
-        }
-    
-        let bookItem = new Book(title, author, pages, readStatus);
-        
-        if(bookItem.title !== tracking[tracking.length-1]){
-            console.log(bookItem.title + '  ' + tracking[tracking.length-1])
-            tracking.push(bookItem.title);
-            console.log(tracking);
-            let li = document.createElement('li'); /* placement necessary to create new li items */
-            li.textContent = bookItem.getBookInfo();
-            output.appendChild(li);
-        }
-        else{
-            alert("Title is already in the library!")
-        }
+        alert("Title is already in the library!");
     }
-    
-        event.preventDefault();
 }
+    function bookOutput(titleItem, authorItem, pagesItem, readItem){  
+        let title = document.createElement('div');
+        title.className = 'title'
+        title.textContent = titleItem;
 
-const delButton = document.querySelector('.delete');
+        let author = document.createElement('div');
+        author.className ='author';
+        author.textContent = authorItem;
 
-delButton.addEventListener('click', (event) => {
-    output.removeChild(-1)
+        let pages = document.createElement('div');
+        pages.className = 'pages';
+        pages.textContent = pagesItem;
 
-    event.preventDefault(); /* THIS IS STILL CRASHING */
-})
+        let deleteButton = document.createElement('button'); /* button under conditional to populate all list items. */
+        deleteButton.className = 'deleteBtn';
+        deleteButton.textContent = 'X';
+
+        let readBtn = document.createElement('button');  
+        readBtn.className = 'readBtn'
+        readBtn.textContent = readItem;
+        changeColor(readBtn);
+        // let color = 'green';
+        // readBtn.style.background = color;
+
+        let li = document.createElement('li');         /* placement necessary to create new li items */
+        li.append(title,author, pages);
+        li.appendChild(deleteButton);
+        li.appendChild(readBtn);
+        output.appendChild(li);
+
+        deleteButton.addEventListener('click',() => {
+            tracking = tracking.filter(item => item !== title.textContent);
+            // filters tracking to remove item with class name === to delBtn click.
+            li.remove();
+            return tracking;
+        })
+
+        readBtn.addEventListener('click', () =>{
+            if(readBtn.textContent === "Read"){
+                readBtn.textContent = 'Not Read';
+            }
+            else{
+                readBtn.textContent = 'Read'
+            }
+            changeColor(readBtn)
+        })
+
+}
+    function changeColor(readBtn){
+        let color = 'rgba(9, 255, 0, 0.288)';
+        if(readBtn.textContent === 'Not Read'){
+            color = 'rgba(255, 0, 0, 0.288)'
+        } 
+        readBtn.style.background = color;  
+    }
+
 
